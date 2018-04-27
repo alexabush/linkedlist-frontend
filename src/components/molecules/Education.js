6703b6ea1602929f2d58e86f06dca524579d767f
Alex Bush <alexabush7@gmail.com>
2018-04-26 18:45:04 -0700

    added company and experience components

---
+++ src/components/molecules/Education.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Education extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <p>Degree from School</p>
+        <p>Year</p>
+      </div>
+    );
+  }
+}
+
+export default Education;
---
+++ src/components/molecules/Experience.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Experience extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <img src="" alt="" />
+        <div>
+          <p>Role + Company Name </p>
+          <p>Time at Company</p>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium a
+          nisi aut cumque vero, recusandae minus explicabo omnis!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Experience;
